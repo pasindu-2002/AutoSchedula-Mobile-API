a7a36e064bcd9589e84ef9f29328b23deaeb690c
pasindu-2002 <pasindualuthwalahewa@gmail.com>
2024-11-29 22:29:03 +0530

    init commit

--- modules.php
+++ modules.php
@@ -21,15 +21,15 @@ function jsonResponse($status, $message, $data = []) {
     exit;
 }
 
-function getCourse($course_code) {
+function getCourse($module_code) {
     $pdo = getDbConnection();
     try {
-        $stmt = $pdo->prepare("SELECT course_name, school FROM course_tbl WHERE course_code = ?");
-        $stmt->execute([$course_code]);
+        $stmt = $pdo->prepare("SELECT module_hours, module_name, course_code FROM modules_tbl WHERE module_code = ?");
+        $stmt->execute([$module_code]);
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
         if ($row) {
-            return ["course_name" => $row['course_name'], "school" => $row['school']];
+            return ["module_name" => $row['module_name'], "module_hours" => $row['module_hours'], "course_code" => $row['course_code'],];
         } else {
             jsonResponse(404, "Course not found or invalid password");
         }
@@ -41,8 +41,8 @@ function getCourse($course_code) {
 function insertCourse($data) {
     $pdo = getDbConnection();
     try {
-        $stmt = $pdo->prepare("INSERT INTO course_tbl (course_code, course_name, school) VALUES (?, ?, ?)");
-        $stmt->execute([$data['course_code'], $data['course_name'], $data['school']]); 
+        $stmt = $pdo->prepare("INSERT INTO modules_tbl (module_code, module_hours, module_name, course_code) VALUES (?, ?, ?, ?)");
+        $stmt->execute([$data['module_code'], $data['module_hours'], $data['module_name'], $data['course_code']]); 
         jsonResponse(201, "Course added successfully");
     } catch (PDOException $e) {
         jsonResponse(500, "Insert failed", ["details" => $e->getMessage()]);
@@ -50,16 +50,16 @@ function insertCourse($data) {
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    if (isset($_GET['course_code'])) {
-        $result = getCourse($_GET['course_code']);
-        jsonResponse(200, "Course fetched successfully", $result);
+    if (isset($_GET['module_code'])) {
+        $result = getCourse($_GET['module_code']);
+        jsonResponse(200, "Module fetched successfully", $result);
     } else {
         jsonResponse(400, "Please provide course code or invalid");
     }
 } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	$input = $_SERVER['REQUEST_METHOD'] === 'POST' ? json_decode(file_get_contents('php://input'), true) : $_GET;
 
-	if (!isset($input['course_code'], $input['course_name'], $input['school'])) {
+	if (!isset($input['module_code'], $input['module_hours'], $input['module_name'])) {
 		jsonResponse(400, "Missing required fields");
 	}
 	
